d1cd2edd037b0d8994cdbf8486025ef898763888
bruhspicy <geraldyywang@gmail.com>
2023-06-26 17:12:56 -0400

    key stored in .env

--- src/apikeys.js
+++ src/apikeys.js
@@ -1,4 +1,4 @@
 // Replace with your WeatherAPI key
-const WEATHERAPI_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXX";
+const WEATHERAPI_API_KEY = process.env.REACT_APP_WEATHERAPI_KEY;
 
 export default WEATHERAPI_API_KEY;
