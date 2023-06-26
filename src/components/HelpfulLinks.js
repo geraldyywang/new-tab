5d5bc6ff7858f719030a920a30b7ace136363c53
bruhspicy <geraldyywang@gmail.com>
2023-06-26 13:58:26 -0400

    Added notion to HelpfulLinks Added hover for links

--- src/components/HelpfulLinks.js
+++ src/components/HelpfulLinks.js
@@ -1,4 +1,4 @@
-import { SiGmail, SiMicrosoftoutlook } from "react-icons/si";
+import { SiGmail, SiMicrosoftoutlook, SiNotion } from "react-icons/si";
 import { GiAcorn, GiFruitTree } from "react-icons/gi";
 import { FaGoogleDrive, FaYoutube } from "react-icons/fa";
 
@@ -6,22 +6,49 @@ const HelpfulLinks = () => {
   return (
     <div className="flex flex-col items-center justify-center px-5 py-5 text-2xl text-neutral-500">
       {/* Quercus, Acorn, Gmail, Drive, Outlook, Youtube */}
-      <a href="https://acorn.utoronto.ca/" className="my-4">
+      <a
+        href="https://acorn.utoronto.ca/"
+        className="my-4 hover:text-neutral-600"
+      >
         <GiAcorn />
       </a>
-      <a href="https://q.utoronto.ca/" className="my-4">
+      <a href="https://q.utoronto.ca/" className="my-4 hover:text-neutral-600">
         <GiFruitTree />
       </a>
-      <a href="https://outlook.office365.com/mail/" className="my-4">
+
+      {/* Replace with your notion link to TODO or what's desired */}
+      <a
+        href="https://www.notion.so/f9a6a338e0404893b26e1a7c280c3f82?v=988ed6935b4e4bb2b20282080773e057"
+        className="my-4 hover:text-neutral-600"
+      >
+        <SiNotion />
+      </a>
+
+      <a
+        href="https://outlook.office365.com/mail/"
+        className="my-4 hover:text-neutral-600"
+      >
         <SiMicrosoftoutlook />
       </a>
-      <a href="https://mail.google.com/mail/u/0/#inbox" className="my-4">
+
+      <a
+        href="https://mail.google.com/mail/u/0/#inbox"
+        className="my-4 hover:text-neutral-600"
+      >
         <SiGmail />
       </a>
-      <a href="https://drive.google.com/drive/u/0/my-drive" className="my-4">
+
+      <a
+        href="https://drive.google.com/drive/u/0/my-drive"
+        className="my-4 hover:text-neutral-600"
+      >
         <FaGoogleDrive />
       </a>
-      <a href="https://www.youtube.com/" className="my-4">
+
+      <a
+        href="https://www.youtube.com/"
+        className="my-4 hover:text-neutral-600"
+      >
         <FaYoutube />
       </a>
     </div>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,19 +5,25 @@ const Socials = () => {
   return (
     <div className="flex flex-row px-5 py-5 text-2xl text-neutral-500 justify-center">
       {/* GitHub, LinkedIn, Instagram, Twitter, Reddit, etc. */}
-      <a href="https://github.com/" className="mx-4">
+      <a href="https://github.com/" className="mx-4 hover:text-neutral-600">
         <FiGithub />
       </a>
-      <a href="https://www.linkedin.com/feed/" className="mx-4">
+      <a
+        href="https://www.linkedin.com/feed/"
+        className="mx-4 hover:text-neutral-600"
+      >
         <FiLinkedin />
       </a>
-      <a href="https://www.instagram.com/" className="mx-4">
+      <a
+        href="https://www.instagram.com/"
+        className="mx-4 hover:text-neutral-600"
+      >
         <FiInstagram />
       </a>
-      <a href="https://twitter.com/" className="mx-4">
+      <a href="https://twitter.com/" className="mx-4 hover:text-neutral-600">
         <FiTwitter />
       </a>
-      <a href="https://www.reddit.com/" className="mx-4">
+      <a href="https://www.reddit.com/" className="mx-4 hover:text-neutral-600">
         <AiOutlineReddit />
       </a>
     </div>
